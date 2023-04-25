ffa0c0f35c957d80bba8b750268785dcbec89f2a
jamiem64 <jmccay@flinder.co>
2023-04-25 16:15:34 +0100

    sync

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import { CCard, CCardBody, CCardHeader, CCol, CRow } from '@coreui/react';
+import "/Users/jamiemccay/Desktop/Personal/Projects/OuterHeaven/frontend/src/App.css";
+
 
 function Home() {
     return (
-        <h1>this is the Home page</h1>
+        <div>
+            {/* <div className='PageHeaderBar'>
+                <div className='PageHeader'>Climate Track</div>
+            </div>
+            <div> */}
+            <CRow>
+              <CCol xs="12" sm="6" md="4">
+                <CCard>
+                  <CCardHeader>Tile 1</CCardHeader>
+                  <CCardBody>Content of Tile 1</CCardBody>
+                </CCard>
+              </CCol>
+              <CCol xs="12" sm="6" md="4">
+                <CCard>
+                  <CCardHeader>Tile 2</CCardHeader>
+                  <CCardBody>Content of Tile 2</CCardBody>
+                </CCard>
+              </CCol>
+              <CCol xs="12" sm="6" md="4">
+                <CCard>
+                  <CCardHeader>Tile 3</CCardHeader>
+                  <CCardBody>Content of Tile 3</CCardBody>
+                </CCard>
+              </CCol>
+            </CRow>
+
+            </div>
+        // </div>
+   
     );
 }
 
