536848a17143289cc9ad09346192109caedc0aea
jamiem64 <jmccay@flinder.co>
2023-04-27 17:58:53 +0100

    Error seemingly fixed

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -9,9 +9,104 @@ import StackedBarChart from "/Users/jamiemccay/Desktop/Personal/Projects/OuterHe
 // "/Users/jamiemccay/Desktop/Personal/Projects/OuterHeaven/frontend/src/components/charts/StackedBarChart.jsx"
 import PieChart from '/Users/jamiemccay/Desktop/Personal/Projects/OuterHeaven/frontend/src/components/charts/PieChart.jsx';
 
-
+import kpi_json_file from '/Users/jamiemccay/Desktop/Personal/Projects/OuterHeaven/frontend/src/data/kpi_json_file.json';
+import selectable_fin_years from '/Users/jamiemccay/Desktop/Personal/Projects/OuterHeaven/frontend/src/data/selectable_fin_years.json';
+import { useEffect, useState } from 'react';
 
 function Home() {
+  // const [kpiData, setKpiData] = React.useState(kpi_json_file);
+  // console.log('kpiData', kpiData);
+
+  const [finYearsSelected, setFinYearsSelected] = React.useState("2022/23");
+
+  // const [kpiValue1, setKpiValue1] = React.useState(kpi_json_file.kpi_1_output.values[finYearsSelected.value]);
+  const [kpiValue1, setKpiValue1] = React.useState(0);
+  // const [kpiValue1New, setKpiValue1New] = React.useState(0);
+  // const [kpiValue1, setKpiValue1] = React.useState(kpiData.kpi_1_output.values["2022/23"]);
+  // const [kpiValue2, setKpiValue2] = React.useState(0);
+  // const [kpiValue3, setKpiValue3] = React.useState(0);
+
+  const [finYearsItems, setFinYearsItems] = React.useState([]);
+
+  const [finYearPanel, setFinYearPanel] = React.useState("2022/23");
+
+  // const [ready, setReady] = React.useState(true);
+
+  // const [submitClicked, setSubmitClicked] = React.useState(false);
+  // const toggleSubmitClicked = () => setSubmitClicked(!submitClicked);
+
+  useEffect (() => {
+    if (finYearsItems.length === 0) {
+      setFinYearsItems(selectable_fin_years);
+    }
+  }, []);
+
+  useEffect (() => {
+    console.log('finYearsItems', finYearsItems)
+  }, [finYearsItems]);
+
+  function submitButton() {
+    setFinYearsSelected(finYearPanel);
+    // toggleSubmitClicked();
+  }
+
+  useEffect (() => {
+    console.log('finYearsSelected', finYearsSelected);
+    console.log('kpi_json_file.kpi_1_output.values[finYearsSelected.value]', kpi_json_file.kpi_1_output.values[finYearsSelected]);
+
+    setKpiValue1(kpi_json_file.kpi_1_output.values[finYearsSelected]);
+  }, [finYearsSelected]);
+
+  useEffect (() => {
+    console.log('finYearPanel', finYearPanel)
+  }, [finYearPanel]);
+
+
+  // const [options, setOptions] = useState([]);
+  // useEffect (() => {
+  //   const getOptions = async () => {
+  //     try {
+  //       const response = await fetch("/Users/jamiemccay/Desktop/Personal/Projects/OuterHeaven/frontend/src/data/kpi_json_file.json");
+  //       const options = await response.json();
+  //       console.log(options);
+  //       setOptions(
+  //         options.map(({ id, jurisdiction, code }) => ({
+  //           id,
+  //           label: jurisdiction,
+  //           value: code
+  //         }))
+  //       );
+  //     } catch (error) {
+  //       // ignore
+  //     }
+  //   };
+  //   getOptions();
+  // }, [finYearPanel]);
+  // useEffect (() => {
+  //   console.log('options', options)
+  // }, [options]);
+
+
+  // function getValue(periodStr) {
+  //   let value = kpiData.kpi_1_output.values[periodStr];
+  //   return value;
+  // }
+
+  // function updateValues() {
+  //   setKpiValue1(getValue(finYearsSelected.value));
+  // }
+
+  // useEffect (() => {
+  //   updateValues();
+  // }, [finYearsSelected]);
+
+  // useEffect (() => {
+  //   setKpiValue1(getValue(finYearPanel.value));
+  //   setKpiValue2(kpiData.kpi_2_output.values[finYearPanel.value]);
+  //   setKpiValue3(kpiData.kpi_3_output.values[finYearPanel.value]);
+  // }, [finYearPanel]);
+  
+
     return (
         <div>
             <div className='PageHeaderBar'>
@@ -29,16 +124,15 @@ function Home() {
                               <div className='ControlPanelFilterText'>Year:</div>
                               <div className='ControlPanelFilterPanel'>
                                 <CFormSelect 
-                                  options={[
-                                    { label: '2019/20', value: '2019/20' },
-                                    { label: '2020/21', value: '2020/21' },
-                                    { label: '2021/22', value: '2021/22' }
-                                  ]}
+                                  options={finYearsItems}
+                                  // value={finYearPanel}
+                                  // value={finYearPanel}
+                                  onChange={(e) => setFinYearPanel(e.currentTarget.value)}
                                 />
                               </div>
                             </div>
                             <div className='SubmitButtonPanel'>
-                                <CButton color="primary">Submit</CButton>
+                                <CButton color="primary" onClick={submitButton}>Submit</CButton>
                             </div>
                           </div>
                         </CCardBody>
@@ -52,19 +146,19 @@ function Home() {
                     <CCol>
                       <CCard className='KPITile'>
                         <CCardHeader className='TileTitle'>Avg. Recycle Rate % (Household)</CCardHeader>
-                        <CCardBody className='KPITileText'>56.7%</CCardBody>
+                        <CCardBody className='KPITileText'>{kpiValue1}%</CCardBody>
                       </CCard>
                     </CCol>
                     <CCol>
                       <CCard className='KPITile'>
                         <CCardHeader className='TileTitle'>Avg. Recycle Rate % (Local Authority)</CCardHeader>
-                        <CCardBody className='KPITileText'>49.2%</CCardBody>
+                        <CCardBody className='KPITileText'>%</CCardBody>
                       </CCard>
                     </CCol>
                     <CCol>
                       <CCard className='KPITile'>
                         <CCardHeader className='TileTitle'>Landfill Waste Per Household (Tonnes)</CCardHeader>
-                        <CCardBody className='KPITileText'>23.7</CCardBody>
+                        <CCardBody className='KPITileText'></CCardBody>
                       </CCard>
                     </CCol>
                   </CRow>
